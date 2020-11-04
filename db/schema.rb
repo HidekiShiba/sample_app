73f74f27b80a155dbf93ad60d9e65d3b41df42a7
EC2 Default User <ec2-user@ip-172-31-25-38.ec2.internal>
2020-11-04 05:43:42 +0000

    [Add] Listモデルに画像カラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_02_121753) do
+ActiveRecord::Schema.define(version: 2020_11_04_053132) do
 
   create_table "lists", force: :cascade do |t|
     t.string "title"
     t.string "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image_id"
   end
 
 end
